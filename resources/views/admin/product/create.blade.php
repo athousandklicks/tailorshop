dfed0a54a1db74fcbc1c42062ab7b90eb0483dcd
Ome Itotoh <omescroll@gmail.com>
2017-06-05 22:05:04 +0100

    recoded how product images are saved locally and on the DB

--- resources/views/admin/product/create.blade.php
+++ resources/views/admin/product/create.blade.php
@@ -47,6 +47,13 @@
 
             <div class="row">
               <div class="form-group col-sm-9 col-xs-12">
+              {{Form::label('reference_no', 'Enter Unique Product Reference No. *')}}
+                {{Form::text('reference_no', null, ['class' => 'form-control', 'placeholder'=>'Enter Product Unique No. This no is not editable after creation'])}}
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="form-group col-sm-9 col-xs-12">
                 {{Form::label('description', 'Description *')}}
                 {{Form::textarea('description', null, ['class' => 'form-control', 'placeholder'=>'Enter Product\'s Full Description', 'rows' => 5])}}
               </div>
@@ -77,85 +84,85 @@
               </div>
             </div> 
       --}}
--->
-            <div class="row">
-              <div class="form-group col-sm-9 col-xs-12">
-              {{ Form::label('colours', 'Choose Colours * (Select Multiple Colours)') }}
-                <select class="form-control colours" name="colours[]" multiple="multiple">
-                  @foreach($colours as $colour)
-                  <option value='{{ $colour->id }}'>{{ $colour->name }}</option>
-                  @endforeach
-                </select>
-              </div>
-            </div>
-
-            <hr>
-
-            <div class="row">
-              <div class="col-sm-12 col-xs-12">
-                <div class="border-left"><h4>Enter Quantity of Each Size</h4></div>
-              </div>
-            </div>
-
-
-            <div class="row">
-              <div class="form-group col-sm-9 col-xs-12">
-                <div class="dateofbirth">
-
-                  {{Form::text('small', null, ['class' => 'form-control', 'placeholder'=>'Small'])}}
-                </div>
-                <div class="dateofbirth">
-                  {{Form::text('medium', null, ['class' => 'form-control', 'placeholder'=>'Medium'])}}
-                </div>
-                <div class="dateofbirth">
-                  {{Form::text('large', null, ['class' => 'form-control', 'placeholder'=>'Large'])}}
-                </div>
-                <div class="dateofbirth">
-                  {{Form::text('xlarge', null, ['class' => 'form-control', 'placeholder'=>'X Large'])}}
-                </div>
-                <div class="dateofbirth">
-                  {{Form::text('xxlarge', null, ['class' => 'form-control', 'placeholder'=>'XX Large'])}}
-                </div>
-              </div>
-            </div>
-
-            <hr>
-            <div class="row">
-              <div class="col-sm-12 col-xs-12">
-                <div class="border-left"><h4>Product Images</h4></div>
-              </div>
-            </div>
-            <div class="row">
-              <div class="form-group col-sm-6 col-xs-12">
-               {{Form::label('front_image', 'Upload Product\'s Front View Image *')}}
-               {{Form::file('front_image', ['class'=>'btn btn-default'])}}
-             </div>
-           </div>
-           <div class="row">
-            <div class="form-group col-sm-6 col-xs-12">
-             {{Form::label('back_image', 'Upload Product\'s Back View Image *')}}
-             {{Form::file('back_image', ['class'=>'btn btn-default'])}}
-           </div>
-         </div>
-         <div class="row">
-          <div class="form-group col-sm-6 col-xs-12">
-           {{Form::label('left_image', 'Upload Product\'s Left-side View Image *')}}
-           {{Form::file('left_image', ['class'=>'btn btn-default'])}}
-         </div>
-       </div>
-       <div class="row">
-        <div class="form-group col-sm-6 col-xs-12">
-         {{Form::label('right_image', 'Upload Product\'s Right-side View Image *')}}
-         {{Form::file('right_image', ['class'=>'btn btn-default'])}}
-       </div>
-     </div>
-
-
-   <div class="row">
-    <div class="form-group col-sm-6 col-xs-6 full-widthsx">
-      {{Form::submit('Add Product', ['class'=>'btn btn-primary'])}}
+    -->
+    <div class="row">
+      <div class="form-group col-sm-9 col-xs-12">
+        {{ Form::label('colours', 'Choose Colours * (Select Multiple Colours)') }}
+        <select class="form-control colours" name="colours[]" multiple="multiple">
+          @foreach($colours as $colour)
+          <option value='{{ $colour->id }}'>{{ $colour->name }}</option>
+          @endforeach
+        </select>
+      </div>
     </div>
+
+    <hr>
+
+    <div class="row">
+      <div class="col-sm-12 col-xs-12">
+        <div class="border-left"><h4>Enter Quantity of Each Size</h4></div>
+      </div>
+    </div>
+
+
+    <div class="row">
+      <div class="form-group col-sm-9 col-xs-12">
+        <div class="dateofbirth">
+
+          {{Form::text('small', null, ['class' => 'form-control', 'placeholder'=>'Small'])}}
+        </div>
+        <div class="dateofbirth">
+          {{Form::text('medium', null, ['class' => 'form-control', 'placeholder'=>'Medium'])}}
+        </div>
+        <div class="dateofbirth">
+          {{Form::text('large', null, ['class' => 'form-control', 'placeholder'=>'Large'])}}
+        </div>
+        <div class="dateofbirth">
+          {{Form::text('xlarge', null, ['class' => 'form-control', 'placeholder'=>'X Large'])}}
+        </div>
+        <div class="dateofbirth">
+          {{Form::text('xxlarge', null, ['class' => 'form-control', 'placeholder'=>'XX Large'])}}
+        </div>
+      </div>
+    </div>
+
+    <hr>
+    <div class="row">
+      <div class="col-sm-12 col-xs-12">
+        <div class="border-left"><h4>Product Images</h4></div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="form-group col-sm-6 col-xs-12">
+       {{Form::label('front_image', 'Upload Product\'s Front View Image *')}}
+       {{Form::file('front_image', ['class'=>'btn btn-default'])}}
+     </div>
+   </div>
+   <div class="row">
+    <div class="form-group col-sm-6 col-xs-12">
+     {{Form::label('back_image', 'Upload Product\'s Back View Image *')}}
+     {{Form::file('back_image', ['class'=>'btn btn-default'])}}
+   </div>
+ </div>
+ <div class="row">
+  <div class="form-group col-sm-6 col-xs-12">
+   {{Form::label('left_image', 'Upload Product\'s Left-side View Image *')}}
+   {{Form::file('left_image', ['class'=>'btn btn-default'])}}
+ </div>
+</div>
+<div class="row">
+  <div class="form-group col-sm-6 col-xs-12">
+   {{Form::label('right_image', 'Upload Product\'s Right-side View Image *')}}
+   {{Form::file('right_image', ['class'=>'btn btn-default'])}}
+ </div>
+</div>
+
+
+<div class="row">
+  <div class="form-group col-sm-6 col-xs-6 full-widthsx">
+    {{Form::submit('Add Product', ['class'=>'btn btn-primary'])}}
   </div>
+</div>
 </fieldset>
 {!! Form::close() !!}
 
